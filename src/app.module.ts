188111430529090d4be3d377da6ea9f62f812c0e
Kaydarin <zim50_killerz@yahoo.com>
2020-11-12 16:24:05 +0800

    Integrated sequelize into project.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import Models from './database/models/index';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, AuthModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      models: Models,
+    }),
+    UsersModule,
+    AuthModule,
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
